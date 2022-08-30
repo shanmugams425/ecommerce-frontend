eb75c961e12bc7c34a3eb86eddb344efa11f369a
shanmugam R <shanmugams425@gmail.com>
2022-08-30 13:54:20 +0530

    aa

--- src/API/CategoriesAPI.js
+++ src/API/CategoriesAPI.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CategoriesAPI() {
-    const [categories, setCategories] = useState([])
-    const [callback, setCallback] = useState(false)
+  const [categories, setCategories] = useState([]);
+  const [callback, setCallback] = useState(false);
 
-    useEffect(() => {
-        const getCategories = async () => {
-            const response = await axios.get(`https://shoppeecom.herokuapp.com/api/category`)
-            setCategories(response.data)
-        }
-        getCategories()
-    }, [callback])
-    return {
-        categories: [categories, setCategories],
-        callback: [callback, setCallback]
-    }
+  useEffect(() => {
+    const getCategories = async () => {
+      const response = await axios.get(
+        `https://ecomerce234.herokuapp.com/api/category`
+      );
+      setCategories(response.data);
+    };
+    getCategories();
+  }, [callback]);
+  return {
+    categories: [categories, setCategories],
+    callback: [callback, setCallback],
+  };
 }
